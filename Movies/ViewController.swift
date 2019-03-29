c081979aef94fbd6ae109b2427c0253164c2a0eb
Juan José Villegas <jj.villegas47@gmail.com>
2019-03-28 20:13:58 -0500

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Juan Jose Villegas on 3/28/19.
+//  Copyright © 2019 Rappi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
